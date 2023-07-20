19bb60577407c2ebf91dd497cd99a356b77d6887
Jose Rosas <josejrrosas@yahoo.com>
2023-07-19 20:59:10 -0500

    added routes to userController and error middleware to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
+import {notFound, errorHandler} from "./middleware/errorMiddleware"
+
 
 dotenv.config();
 
@@ -11,5 +13,8 @@ const app = express();
 app.use("/api/users", userRoutes)
 
 app.get('/', (req,res) => res.send('server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
